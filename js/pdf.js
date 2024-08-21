92708cc04c557d3db8f022088dd0cf9c55b4a0d5
welton1995 <estudanteativo198@gmail.com>
2024-08-21 16:37:22 -0300

    teste

--- js/pdf.js
+++ js/pdf.js
@@ -1,23 +1,26 @@
 const btnGenerate = document.querySelector('#generate-pdf');
 const conteudo = document.querySelector('#conteudo');
 const data = new Date();
-const nomeRelatório = data.toLocaleDateString("pt-BR", { year:'numeric', month: 'numeric', day: 'numeric' })
+const nomeRelatório = data.toLocaleDateString("pt-BR", { year:'numeric', month: 'numeric', day: 'numeric' });
 
-btnGenerate.addEventListener('click', async ()=> {
+btnGenerate.addEventListener('click', async () => {
+  const screenWidth = window.innerWidth;
+  const isMobile = screenWidth <= 768; // Verifica se é um dispositivo móvel
+  const scale = isMobile ? 0.7 : 1; // Ajusta a escala para dispositivos móveis
+
   const options = {
-  margin: [2, 2, 2, 2],
-  filename: `Relatório_Estoque_${nomeRelatório}.pdf`,
-  html2canvas: {scale: 2},
-  jsPDF: {unit: 'mm', format: 'a4', orientation: 'landscape'}
-}
+    margin: [2, 2, 2, 2],
+    filename: `Relatório_Estoque_${nomeRelatório}.pdf`,
+    html2canvas: { scale: scale },
+    jsPDF: { unit: 'mm', format: 'a4', orientation: isMobile ? 'portrait' : 'landscape' }
+  }
 
-// Gerar pdf
-await html2pdf().set(options).from(conteudo).save();
+  // Gerar pdf
+  await html2pdf().set(options).from(conteudo).save();
 
-Swal.fire({
-  title: "PDF gerado com sucesso!",
-  icon: "success",
-  confirmButtonColor: "#0275d8",
-});
-
+  Swal.fire({
+    title: "PDF gerado com sucesso!",
+    icon: "success",
+    confirmButtonColor: "#0275d8",
+  });
 });
