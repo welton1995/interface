5b49bc84e94b0e28b1aa4508912fbf38712754d2
welton1995 <estudanteativo198@gmail.com>
2024-08-21 17:34:23 -0300

    teste

--- js/pdf.js
+++ js/pdf.js
@@ -9,6 +9,12 @@ btnGenerate.addEventListener('click', async () => {
   // Forçar tamanho menor da fonte
   conteudo.style.fontSize = '12px'; // Ajuste para o tamanho desejado
 
+  await Swal.fire({
+    title: "PDF gerado com sucesso!",
+    icon: "success",
+    confirmButtonColor: "#0275d8",
+  });
+
   const options = {
     margin: [2, 2, 2, 2],
     filename: `Relatório_Estoque_${nomeRelatório}.pdf`,
@@ -31,11 +37,6 @@ btnGenerate.addEventListener('click', async () => {
   // Reverter o tamanho da fonte ao original
   conteudo.style.fontSize = originalFontSize;
 
-  await Swal.fire({
-    title: "PDF gerado com sucesso!",
-    icon: "success",
-    confirmButtonColor: "#0275d8",
-  });
 });
 
 
